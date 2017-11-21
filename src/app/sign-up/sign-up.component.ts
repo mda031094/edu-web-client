d5e27cf21818a94a3b84f516ca21358ec95c9bb6
beltasha <natalya.belyaeva@yumasoft.com>
2017-11-21 16:23:43 +0300

    add fields to sign in component for production mode

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -10,6 +10,12 @@ export class SignUpComponent {
 
   constructor(private router: Router) { }
 
+    public firstname: string;
+    public lastname: string;
+    public email: string;
+    public password: string;
+    public confirmPassword: string;
+
     public signUp(): void {
         this.router.navigate(['main']);
     }
